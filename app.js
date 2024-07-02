ffffa194e8d248e3fa2df3567c2ecd936513c35b
Muugii9902 <muugii9902@gmail.com>
2024-07-02 12:29:39 +0800

    todo

--- app.js
+++ app.js
@@ -6,19 +6,19 @@ const addTaskBtn = document.getElementById("addTaskBtn");
 const saveBtn = document.getElementById("save-btn");
 const taskInput = document.getElementById("task-input");
 const taskStatus = document.getElementById("status");
-const trashBtn = document.getElementById("btn2");
+// const trashBtn = document.getElementById("btn2");
 // const task = [];
 
 // VARIABLES FOR TASK
-const tasks = [
-  {
-    name: "Task Two",
-    status: "INPROGRESS",
-  },
-  {
-    name: "Task Three",
-    status: "BLOCKED",
-  },
+let tasks = [
+  // {
+  //   name: "Task Two",
+  //   status: "INPROGRESS",
+  // },
+  // {
+  //   name: "Task Three",
+  //   status: "BLOCKED",
+  // },
 ];
 
 function zurah() {
@@ -29,15 +29,40 @@ function zurah() {
 
   for (let i = 0; i < tasks.length; i++) {
     console.log("TASKS", tasks);
+    let borderColor = "";
+    switch (tasks[i].status) {
+      case "TODO": {
+        borderColor = "";
+        break;
+      }
+      case "INPROGRESS": {
+        borderColor = "border-primary";
+
+        break;
+      }
+      case "DONE": {
+        borderColor = "border-success-subtle";
+        break;
+      }
+      case "BLOCKED": {
+        borderColor = "border-warning";
+        break;
+      }
+      default: {
+        console.log("ALDAA GARLAA");
+      }
+    }
+
     const newTaskCard = `
-    <div class="d-flex justify-content-between align-items-center border border-1 rounded p-2">
-    <span>${tasks[i].name}</span>
+    <div class="d-flex justify-content-between align-items-center border border-1   rounded p-2 ${borderColor}">
+    <span>${tasks[i].name} - </span>
     <div>
-        <button id ="bnt1">
+        <button data-bs-toggle="modal"
+          data-bs-target="#taskModal" class ="bnt1" onclick="moveTask(${i})"> 
         <i class="bi bi-pencil"></i>
         </button>
-        <button id ="btn2">
-        <i class="bi bi-trash">${tasks[i].value}</i>
+        <button class ="btn2">
+        <i class="bi bi-trash" onclick="deleteTask(${i})"></i>
         </button>
     </div>
     </div>
@@ -66,7 +91,6 @@ function zurah() {
     }
   }
 }
-
 saveBtn.addEventListener("click", function () {
   const newTask = {
     name: taskInput.value,
@@ -81,14 +105,26 @@ zurah();
 // trashBtn.addEventListener("click", function () {
 //   let index = tasksArray.findIndex();
 // });
-trashBtn.addEventListener("click", function () {
-  const taskToRemove = taskInput.value;
-  const index = tasks.findIndex((task) => task.name === taskToRemove);
-  if (index > -1) {
-    tasks.splice(index, 1);
-    zurah();
-    console.log("TASKS", tasks);
-  } else {
-    console.log("Task not found");
-  }
-});
+// trashBtn.addEventListener("click", function () {
+//   const taskToRemove = taskInput.value;
+//   const index = tasks.findIndex((task) => task.name === taskToRemove);
+//   if (index > -1) {
+//     tasks.splice(index, 1);
+//     zurah();
+//     console.log("TASKS", tasks);
+//   } else {
+//     console.log("Task not found");
+//   }
+// });
+const deleteTask = (taskIndex) => {
+  tasks.splice(taskIndex, 1);
+  zurah();
+  console.log("task delete", taskIndex);
+};
+// const editTask = (taskIndex) => {};
+const moveTask = (taskIndex) => {
+  taskInput.value = tasks[taskIndex].name;
+  taskStatus.value = tasks[taskIndex].status;
+  tasks.splice(taskIndex, 1);
+  zurah();
+};
